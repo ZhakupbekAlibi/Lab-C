ba889eccbb5381a87defb4ec790ba692d69393bf
ZhakupbekAlibi <ZhakupbekAlibi@gmail.com>
2021-04-30 22:07:54 +0600

    commit

---
+++ MVCCore/MVCCore/MVCCore/Data/MvcMovieContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCCore.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
